010c2178bb11942ca045602dfa42c7976fcbe12c
Babloo <babloo.1@tcs.com>
2021-04-17 16:30:41 +0200

    Project configuration done with git repository.

---
+++ casinoSystem/src/main/java/com/local/rank/casinoSystem/CasinoApplication.java
@@ -0,0 +1,13 @@
+package com.local.rank.casinoSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class CasinoApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
